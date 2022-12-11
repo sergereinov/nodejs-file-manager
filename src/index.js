beffd56ce19062ab5adf6389bbbc71a01aef5bda
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-11 16:29:22 +0300

    feat: parse args into object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+import { parseArgs } from './args.js'
+
+const args = parseArgs(process.argv.slice(2));
+
+function exit() {
+    console.log(`Thank you for using File Manager, ${args.username}, goodbye!`)
+    process.exit();
+}
+
+console.log(`Welcome to the File Manager, ${args.username}!`);
+
+//TODO the app
+
+exit();
