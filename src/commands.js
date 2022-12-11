9f4a80037f51ad31e826afaa80ce3d4704469781
sergereinov <47950629+sergereinov@users.noreply.github.com>
2022-12-11 21:24:02 +0300

    feat: implement commands processor

---
+++ src/commands.js
@@ -0,0 +1,16 @@
+export class Commands {
+    constructor(commands) {
+        this.commands = commands;
+    }
+
+    do(workdir, line) {
+        const cmdLen = line.indexOf(' ');
+        const cmd = (cmdLen > 0) ? line.substring(0, cmdLen) : line;
+        const action = this.commands[cmd];
+        if (typeof (action) === 'function') {
+            const params = line.substring(cmdLen + 1);
+            return action(workdir, params);
+        }
+        return false;
+    }
+};
